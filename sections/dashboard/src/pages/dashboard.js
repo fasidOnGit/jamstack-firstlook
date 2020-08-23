2c75a9d0a525a3e4a34ceed221262eeada43123b
Kader Fasid <fasidmpm@gmail.com>
2020-08-23 19:34:07 +0530

    Authenticated Dashboard site complete

--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Router } from '@reach/router';
 import Layout from '../components/layout';
 import Profile from '../components/profile';
@@ -6,21 +6,30 @@ import RouteBase from '../components/route-base';
 import RouteSecret from '../components/route-secret';
 import RouteLogin from '../components/route-login';
 import { navigate } from 'gatsby';
+import IdentityModal from 'react-netlify-identity-widget';
+
+import 'react-netlify-identity-widget/styles.css';
+import PrivateRoute from '../components/private-route';
 
 const Dashboard = ({ location }) => {
+    const [isVisible, setVisibilty] = useState(false);
     useEffect(() => {
         if (location.pathname.match(/^\/dashboard\/?$/)) {
             navigate('/dashboard/login', { replace: true});
         }
-    }, [location.pathname])
+    }, [location.pathname]);
+
+    const showModal = () => setVisibilty(true);
+
     return (
         <Layout>
-            <Profile />
+            <Profile showModal={showModal} />
             <Router>
-                <RouteBase path="/dashboard/base" />
-                <RouteSecret path="/dashboard/secret" />
-                <RouteLogin path="/dashboard/login" />
+                <PrivateRoute path="/dashboard/base" component={RouteBase} />
+                <PrivateRoute path="/dashboard/secret" component={RouteSecret} />
+                <RouteLogin path="/dashboard/login" showModal={showModal}/>
             </Router>
+            <IdentityModal showDialog={isVisible} onCloseDialog={() => setVisibilty(false)} />
         </Layout>
     )
 }
